0ea548687cac4de15ddc6d782a98e83ad062cee2
miiicmueller <michael.muller@he-arc.ch>
2014-11-13 15:53:26 +0100

    Compile version

--- chronometre.c
+++ chronometre.c
@@ -28,6 +28,53 @@ static struct stopwatch stopwatch;
 static gptDevStruct gGpt2;
 static gptDevStruct gGpt3;
 
+static irqreturn_t gpio_start(int irq, void* handle)
+    {
+    struct stopwatch* oref = (struct stopwatch*) handle;
+    oref->function = start;
+    return IRQ_HANDLED;
+    }
+irqreturn_t gpio_stop(int irq, void* handle)
+    {
+    struct stopwatch* oref = (struct stopwatch*) handle;
+    oref->function = stop;
+    return IRQ_HANDLED;
+    }
+irqreturn_t gpio_reset(int irq, void* handle)
+    {
+    struct stopwatch* oref = (struct stopwatch*) handle;
+    oref->function = reset;
+    return IRQ_HANDLED;
+    }
+
+irqreturn_t ISR_GPT2_thread(int irq, void *dev_id)
+    {
+    gptDevStruct *gGpt = (gptDevStruct*) dev_id;
+    debugprint("GPT 2 Thread");
+    if (gGpt->stopWatch->function == start)
+	gGpt->stopWatch->is_counting_enabled = true;
+    if (gGpt->stopWatch->function == reset)
+	{
+	gGpt->stopWatch->is_counting_enabled = false;
+	gGpt->stopWatch->counter_value = 0;
+	}
+    if (gGpt->stopWatch->is_counting_enabled)
+	gGpt->stopWatch->counter_value++;
+    if (gGpt->stopWatch->function != stop)
+	gGpt->stopWatch->display_value = gGpt->stopWatch->counter_value;
+    return IRQ_HANDLED;
+    }
+
+irqreturn_t ISR_GPT3_thread(int irq, void *dev_id)
+    {
+    gptDevStruct *gGpt = (gptDevStruct*) dev_id;
+    debugprint("GPT 3 Thread");
+    display_seg7(gGpt->stopWatch->display_value, gGpt->stopWatch->is_left);
+    debugprint("Gptt left = %d", gGpt->stopWatch->is_left);
+    gGpt->stopWatch->is_left = (bool) !gGpt->stopWatch->is_left;
+    return IRQ_HANDLED;
+    }
+
 /**
  * Driver input function
  */
@@ -53,8 +100,7 @@ static int __init mlaboChrono_init(void)
     mGpt_Open(&gGpt2, 100);
     mGpt_Open(&gGpt3, 10);
 
-
- 	gpio_attach(GPIO_PORT_E, 3, GPIO_IRQ_FALLING, gpio_start, &stopwatch);
+    gpio_attach(GPIO_PORT_E, 3, GPIO_IRQ_FALLING, gpio_start, &stopwatch);
     gpio_enable(GPIO_PORT_E, 3);
     gpio_attach(GPIO_PORT_E, 4, GPIO_IRQ_FALLING, gpio_stop, &stopwatch);
     gpio_enable(GPIO_PORT_E, 4);
@@ -62,7 +108,8 @@ static int __init mlaboChrono_init(void)
     gpio_enable(GPIO_PORT_E, 6);
 
     mGpt_Open(100, 10);
-    mGpt_EnableGpts();
+    mGpt_EnableGpts(&gGpt2);
+    mGpt_EnableGpts(&gGpt3);
 
     debugprint("Real application running...");
 
@@ -75,7 +122,7 @@ static int __init mlaboChrono_init(void)
 static int __exit mlaboChrono_exit(void)
     {
     gpio_cleanup();
-   
+
     mGpt_Release(&gGpt2);
     mGpt_Release(&gGpt3);
 
